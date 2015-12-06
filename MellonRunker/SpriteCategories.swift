354f18c0758752a17e832360d3c8909642d6a8b5
Se-Joon Chung <sejoon210@gmail.com>
2015-12-05 23:20:13 -0500

    Simplify group bit mask categories

--- MellonRunker/SpriteCategories.swift
+++ MellonRunker/SpriteCategories.swift
@@ -11,11 +11,5 @@ import Foundation
 // constants for the physics body bit masks used in contact/collision detection
 let asteroidCategory: UInt32  = 0x1 << 0
 let spaceshipCategory: UInt32 = 0x1 << 1
-let paddleCategory: UInt32 = 0x1 << 2
-let ballCategory: UInt32 = 0x1 << 3
-let blockCategory: UInt32 = 0x1 << 4
-let boundaryCategory: UInt32 = 0x1 << 5
-let farBoundaryCategory: UInt32 = 0x1 << 6
-/*let paddleCategory: UInt32 = 0x1 << 2
-let sheepCategory: UInt32 = 0x1 << 3
-let wolfCategory: UInt32 = 0x1 << 4*/
+let boundaryCategory: UInt32 = 0x1 << 2
+let farBoundaryCategory: UInt32 = 0x1 << 3
